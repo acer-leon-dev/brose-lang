da139b5c733150edafae0ef9946f27aee10906d1
Leon <leonv.allotey@gmail.com>
2025-03-14 13:44:52 -0500

    Update Lexer.cpp

--- Math-Compiler/src/Lexer.cpp
+++ Math-Compiler/src/Lexer.cpp
@@ -3,63 +3,125 @@
 namespace
 {
 
-bool myisspace(char c) 
+struct mctype
 {
-    return c == ' ' || c == '\t' || c == '\v' || c == '\f' || c == '\r';
-}
-
-};
-
-bool iskeyword_log(std::string token)
-{
-    if (!token.starts_with("log")) {
-        return false;
+    static bool isspace(char c) 
+    {
+        return c == ' ' || c == '\t' || c == '\v' || c == '\f' || c == '\r';
     }
 
-    for (char ch : token.substr(4)) {
-        if (!std::isdigit(ch) && ch != '.') {
+    static bool isfloatdigit(char c)
+    {
+        return std::isdigit(c) || c == '.';
+    }
+
+    static bool isalnumfloat(char c)
+    {
+        return std::isalnum(c) || c == '.';
+    }
+    
+}; // struct mctype
+
+struct iskeyword
+{
+    static bool islog(std::string tok)
+    {
+        // return if natural log
+        if (tok == "ln") {
+            return true;
+        }
+        
+        if (!tok.starts_with("log")) {
             return false;
         }
+
+        // verify base
+        for (int i = 4; i < tok.length(); i++) {
+            if (!mctype::isfloatdigit(tok[i])) {
+                    return false;
+            }
+        }
+    
+        return true;
     }
 
-    return true;
-}
+}; // struct iskeyword
 
 std::size_t getToken(const std::string& src, std::size_t pos, Token& dest)
 {
     using enum TokenType;
-
     std::size_t start = pos;
-    std::string tok;
-    TokenType t;
+    
     char lastch = src[start];
-    while (std::isspace(lastch)) {
+    
+    // skip whitespace
+    while (mctype::isspace(lastch)) {
         lastch = src[++pos];
     }
 
+    std::string tok;
+    TokenType t;
+
+    // Process identifiers and keywords
     if (std::isalpha(lastch))
     {
         tok.push_back(lastch);
-        for (lastch = src[++pos]; std::isalnum(lastch) || lastch == '.'; lastch = src[++pos]) {
+        for (lastch = src[++pos]; std::isalnum(lastch); lastch = src[++pos]) {
             tok.push_back(lastch);
         }
 
         if (tok == "mod") {
             t = MOD;
         } else if (tok == "floor") {
-            t =  GENERIC_FUNC;
+            t = GENERIC_FUNC;
         } else if (tok == "ceil") {
             t = GENERIC_FUNC;
-        } else if (iskeyword_log(tok)) {
+        } else if (iskeyword::islog(tok)) {
             t = LOG;
+        } else {
+            t = IDENTIFIER;
+        }
+
+        // Match operators
+        if (tok.length() == 1)
+        {
+            switch (tok[0]) {
+                case '+': t = 0; break;
+                case '-': t = 0; break;
+                case '*': t = 0; break;
+                case '/': t = 0; break;
+                case '^': t = 0; break;
+            }
         }
     }
 
-    // if (tok.starts_with("log"))
+    // Process numbers
+    if (mctype::isfloatdigit(lastch))
+    {
+        t = NUMBER;
+        do {
+            tok.push_back(lastch);
+            lastch = src[++pos];
+        } while (mctype::isfloatdigit(lastch));
+    }
+
+    // Process comments
+    if (lastch == '/')
+    {
+        lastch = src[++pos];
+        if (lastch == '/')
+        {
+            do {
+                lastch = src[++pos];
+            } while (pos < src.length() && lastch != '\n' && lastch != '\r');
+        }
+    }
     
     dest = { t, tok };
     return pos - start;
-}
+} // std::size_t getToken
+
+}; // [anonymous] namespace
 
 std::vector<Token> tokenize(const std::string& src)
 {
@@ -72,4 +134,4 @@ std::vector<Token> tokenize(const std::string& src)
     }
 
     return tokens_list;
-}
+} // std::vector<Token> tokenize
