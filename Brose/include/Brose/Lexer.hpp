fc4fb271d40ebb193cf5e15dc131909d62ab4a00
Leon <leonv.allotey@gmail.com>
2025-03-18 15:30:53 -0500

    convert token enums to flags

--- Brose/include/Brose/Lexer.hpp
+++ Brose/include/Brose/Lexer.hpp
@@ -3,23 +3,23 @@
 
 enum class TokenType : int
 {
-    NOT_A_TOKEN = 0,
-    EOL,
-    VARIABLE,
-    NUMBER,
-    OP_OPEN_PAREN,
-    OP_CLOSE_PAREN,
-    OP_ABS,
-    OP_EQUAL,
-    OP_PLUS,
-    OP_MINUS,
-    OP_MULTIPLY,
-    OP_DIVIDE,
-    OP_EXPONENT,
-    OP_FACTORIAL,
-    OP_MOD,
-    FUNCTION_GENERIC,
-    FUNCTION_LOGARITHM
+    NOT_A_TOKEN = 0x0,
+    EOL = 0x1,
+    VARIABLE = 0x2,
+    NUMBER = 0x4,
+    OP_OPEN_PAREN = 0x8,
+    OP_CLOSE_PAREN = 0x10,
+    OP_ABS = 0x20,
+    OP_EQUAL = 0x40,
+    OP_PLUS = 0x80,
+    OP_MINUS = 0x100,
+    OP_MULTIPLY = 0x200,
+    OP_DIVIDE = 0x400,
+    OP_EXPONENT = 0x800,
+    OP_FACTORIAL = 0x1000,
+    OP_MOD = 0x2000,
+    FUNCTION_GENERIC = 0x4000,
+    FUNCTION_LOGARITHM = 0x8000
 };
 
 struct Token
