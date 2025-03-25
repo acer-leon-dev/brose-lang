af75ee81ce9bafd8016872798c310fced84b4868
leon-acer <leonv.allotey@gmail.com>
2025-03-25 01:25:18 -0500

    make enums look nicer

--- Brose/include/brose/lexer.hpp
+++ Brose/include/brose/lexer.hpp
@@ -8,27 +8,23 @@ namespace brose
 
 enum class TokenType : int
 {
-    EOL          = 1 << 0,
-    Variable     = 1 << 1,
-    Number       = 1 << 2,
-
-    OpenParen    = 1 << 3,
-    CloseParen   = 1 << 4,
-    
-    Assign       = 1 << 5,
-    Plus         = 1 << 6,
-    Minus        = 1 << 7,
-    Multiply     = 1 << 8,
-    Divide       = 1 << 9,
-    Exponent     = 1 << 10,
-    Modulus      = 1 << 11,
-
-    Absolute     = 1 << 12,
-    Factorial    = 1 << 13,
-    
-    NormalFunction           = 1 << 14,
-    LogarithmicFunction      = 1 << 15,
-    TrigonometricFunction    = 1 << 16,
+    EOL                     = 1 <<  0,
+    Variable                = 1 <<  1,
+    Number                  = 1 <<  2,
+    OpenParen               = 1 <<  3,
+    CloseParen              = 1 <<  4,
+    Assign                  = 1 <<  5,
+    Plus                    = 1 <<  6,
+    Minus                   = 1 <<  7,
+    Multiply                = 1 <<  8,
+    Divide                  = 1 <<  9,
+    Exponent                = 1 << 10,
+    Modulus                 = 1 << 11,
+    Absolute                = 1 << 12,
+    Factorial               = 1 << 13,
+    NormalFunction          = 1 << 14,
+    LogarithmicFunction     = 1 << 15,
+    TrigonometricFunction   = 1 << 16,
 
     None    = 0,
     Any     = EOL | Variable | Number | OpenParen 
