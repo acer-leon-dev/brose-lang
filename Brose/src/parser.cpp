2824329dc9db9cbccf1549a63f4b46324e0bc35f
leon-acer <leonv.allotey@gmail.com>
2025-03-26 19:18:05 -0500

    update parser

--- Brose/src/parser.cpp
+++ Brose/src/parser.cpp
@@ -1,175 +1,177 @@
-// #include "pch.hpp"
-// #include <brose/parser.hpp>
-// #include <brose/ast.hpp>
-// #include <brose/lexer.hpp>
-// #include <brose/operations.hpp>
+#include "pch.hpp"
+#include <brose/parser.hpp>
+#include <brose/ast.hpp>
+#include <brose/lexer.hpp>
+#include <brose/operations.hpp>
 
-// using namespace brose;
+namespace {
 
-// // Alias to token vector iterator
-// using TokenVecIt = std::vector<Token>::const_iterator; 
+using namespace brose;
+using token_it_ref = std::vector<Token>::const_iterator&; 
+using expr_node_uptr = std::unique_ptr<ExprNode>;
 
-// namespace
-// {
+int            get_binop_precedence (const Token& token);
+expr_node_uptr log_error            (const std::string& message);
+expr_node_uptr parse_argument       (token_it_ref it);
+expr_node_uptr parse_binop_rhs      (token_it_ref it, int expr_precedence, expr_node_uptr expr_left);
+expr_node_uptr parse_expression     (token_it_ref it);
+expr_node_uptr parse_function_expr  (token_it_ref it);
+expr_node_uptr parse_number_expr    (token_it_ref it);
+expr_node_uptr parse_paren_expr     (token_it_ref it);
+expr_node_uptr parse_primary        (token_it_ref it);
+expr_node_uptr parse_variable_expr  (token_it_ref it);
 
-// operations::function get_binary_function_from_token(const Token& token) {
-//     if (token.type == token_logarithmic_function) {
-//         return operations::log;
-//     }
+int get_binop_precedence(const Token& token) {
+    static const std::unordered_map<TokenType, int>
+    binary_operator_precedence_map = {
+        { token_exponent, 10 }, // E 
+        { token_multiply, 20 }, // M
+        { token_divide,   20 }, // D
+        { token_modulus,  20 },
+        { token_plus,     30 }, // A
+        { token_minus,    30 }, // S
+    };
+    static int token_any_operator   = token_open_paren | token_close_paren | token_assign | token_plus 
+                            | token_minus | token_multiply | token_divide | token_exponent 
+                            | token_modulus | token_absolute | token_factorial;
+    if (token.type & token_any_operator) {
+        return binary_operator_precedence_map.at(token.type);
+    }
+    return -1;
+}
 
-//     throw std::invalid_argument("");
-// }
+expr_node_uptr log_error(const std::string& m) {
+    std::cerr << "Error: " << m << "\n";
+    return nullptr;
+}
 
-// int get_binop_precedence(const Token& token) {
-//     static const std::unordered_map<TokenType, int>
-//     binop_precedence_map = {
-//         { token_exponent, 1 }, // E 
-//         { token_multiply, 2 }, // M
-//         { token_divide,   2 }, // D
-//         { token_modulus,  2 },
-//         { token_plus,     3 }, // A
-//         { token_minus,    3 }, // S
-//     };
+expr_node_uptr parse_binop_rhs(token_it_ref it, int precedence, expr_node_uptr expr_left) {
+    while (true) {
+        // Determine precedence of [this-op]
+        int this_token_precedence = get_binop_precedence(*it);
+        if (this_token_precedence < precedence) {
+            return expr_left; // Return left hand, e.g., [a] + b
+        }
 
-//     int AnyOperator   = token_open_paren | token_close_paren | token_assign | token_plus 
-//                             | token_minus | token_multiply | token_divide | token_exponent 
-//                             | token_modulus | token_absolute | token_factorial;
+        // Evaluate operator type
+        TokenType bin_op = it->type;
+        it++; // Skip operator
 
-//     if (!(token.type & AnyOperator)) {
-//         return -1;
-//     }
+        // Parse right hand expression, e.g., a + [b]
+        auto expr_right = parse_primary(it);
+        if (!expr_right) {
+            return nullptr;
+        }
 
-//     return binop_precedence_map.at(token.type);
-// }
+        // Evaluate the prec. of the next operation
+        int next_token_precedence = get_binop_precedence(*it);
+        // Check if [this-op] has lower prec. than [next-op]
+        // If so...
+        if (this_token_precedence < next_token_precedence) {
+            // ...evaluate [next-expr] first & use result as operand for [this-expr]
+            expr_right = parse_binop_rhs(it, this_token_precedence + 1, std::move(expr_right));
+            if (!expr_right) {
+                return nullptr;
+            }
+        }
 
-// std::unique_ptr<ExprNode> log_error (const std::string& m) {
-//     std::cerr << "Error: " << m << "\n";
-//     return nullptr;
-// }
+        // Finally return this expression when [this-prec] is higher or equal to following precs.
+        expr_left = std::make_unique<BinaryExprNode>(bin_op, std::move(expr_left), std::move(expr_right));
+    }
+}
 
-// std::unique_ptr<ExprNode> parse_binop_rhs(TokenVecIt& it, int expr_precedence, std::unique_ptr<ExprNode> lhs) {
-//     using namespace operations;
-//     while (true) {
-//         int tok_prec = get_binop_precedence(*it);
-//         if (tok_prec < expr_precedence) {
-//             return lhs;
-//         }
+expr_node_uptr parse_expression(token_it_ref it) {
+    auto expr_left = parse_primary(it);
+    return (expr_left) ? parse_binop_rhs(it, 0, std::move(expr_left)) : nullptr;
+}
 
-//         TokenType op = it->type;
-//         it++;
+expr_node_uptr parse_number_expr(token_it_ref it)  {
+    double num = std::stod(it->value);
+    auto result = std::make_unique<NumberExprNode>(num);
+    it++; // skip number
+    return std::move(result);
+}
 
-//         std::unique_ptr<ExprNode> rhs = parse_primary(it);
-//         if (!rhs) {
-//             return nullptr;
-//         }
-
-//         int next_prec = get_binop_precedence(*it);
-//         if (tok_prec < next_prec) {
-//             rhs = parse_binop_rhs(it, tok_prec + 1, std::move(rhs));
-//         }
-
-//         lhs = std::make_unique<BinaryExprNode>(op, std::move(lhs), std::move(rhs));
-//     }
-// }
-
-// std::unique_ptr<ExprNode> parse_expression(TokenVecIt& it) {
-//     auto lhs = parse_primary(it);
-//     if (!lhs) {
-//         return nullptr;
-//     }
-
-//     return parse_binop_rhs(it, 0, std::move(lhs));
-// }
-
-// std::unique_ptr<ExprNode> parse_number_expr(TokenVecIt& it)  {
-//     double num = std::stod(it->value);
-//     auto result = std::make_unique<NumberExprNode>(num);
-//     it++; // skip number
-//     return std::move(result);
-// }
-
-// std::unique_ptr<ExprNode> parse_parenthesized_expr(TokenVecIt& it) {
-//     it++; // skip open paren
-//     auto V = parse_expression(it);
-//     if (!V) {
-//         return nullptr;
-//     }
-
-//     if (it->type != token_close_paren) {
-//         return log_error("expected ')'");
-//     }
-
-//     it++; // skip close paren
-//     return V;
-// }
-
-// std::unique_ptr<ExprNode> parse_variable_expr(TokenVecIt& it) {
-//     std::string name = it->value;
-//     it++; // skip variable
-//     return std::make_unique<VariableExprNode>(name); 
-// }
-
-// std::unique_ptr<ExprNode> parse_argument(TokenVecIt& it) {
-//     std::unique_ptr<ExprNode> arg = parse_expression(it);
-//     it++;
-//     return (arg) ? std::move(arg) : nullptr;
-// }
-
-// std::unique_ptr<ExprNode> parse_function_expr(TokenVecIt& it) {   
-//     std::vector<std::unique_ptr<ExprNode>> args;
+expr_node_uptr parse_paren_expr(token_it_ref it) {
+    it++; // skip open paren
     
-//     std::string name = it->value;
-//     if (it->type & token_normal_function | token_trigonometric_function) {
+    auto V = parse_expression(it);
+    if (!V) {
+        return nullptr;
+    }
+
+    if (it->type != token_close_paren) {
+        return log_error("expected ')'");
+    }
+
+    it++; // skip close paren
+    return V;
+}
+
+expr_node_uptr parse_variable_expr(token_it_ref it) {
+    std::string name = it->value;
+    it++; // skip variable
+    return std::make_unique<VariableExprNode>(std::move(name)); 
+}
+
+expr_node_uptr parse_function_expr(token_it_ref it) { 
+    static int unary_function_flag = token_normal_function | token_trigonometric_function;
+    // static int binary_function_flag = token_logarithmic_function;
+
+    auto node = std::make_unique_for_overwrite<FunctionCallExprNode>();
+
+    if (it->type & token_logarithmic_function) {
+        // Set identifier to generic name
+        node->name = "log";
+        
+        // Get base argument
+        double base = (it->value.length() == 3) ? 10 : std::stod(it->value.substr(4));
+        node->args.push_back(std::make_unique<NumberExprNode>(base));
+    }
     
-//     } else if (it->type & token_logarithmic_function) {
-//         function = get_binary_function_from_token(*it);
-
-//         if (it->type == token_logarithmic_function) {
-//             double base = (it->value.length() == 3) ? 10 : std::stod(it->value.substr(4));
-//             args.push_back(std::make_unique<NumberExprNode>(base));
-//         }
-//     }
-
-//     it++; // skip identifier
-
-//     if (auto arg = parse_expression(it)) {
-//         args.push_back(std::move(arg));
-//     } else {
-//         return nullptr;            
-//     }
+    it++; // Identifier
+    it++; // Open parenthesis
     
-//     if (it->type != token_close_paren) {
-//         return log_error("Expected \')\'");
-//     }
-//     it++; // skip closing paren
-
-//     return std::make_unique<FunctionCallExprNode>(function, std::move(args));
-// }
-
-// std::unique_ptr<ExprNode> parse_primary(TokenVecIt& it) {
-//     switch (it->type) {
-//     case token_variable:
-//         return parse_variable_expr(it);
-//     case token_number:
-//         return parse_number_expr(it);
-//     case token_open_paren:
-//         return parse_parenthesized_expr(it);
-//     }
+    // Get next argument
+    if (auto arg = parse_expression(it)) {
+        node->args.push_back(std::move(arg));
+    } else {
+        return nullptr;       
+    }
     
-//     if (static_cast<int>(it->type) & static_cast<int>(token_any)) {
-//         return parse_function_expr(it);
-//     }
-
-//     return log_error(std::format("unknown token \"{}\", expected an expression", it->value));
-// }
-
-// };
-
-// namespace brose
-// {
-
-// void parse(const std::vector<Token>& tokens) {
+    // Check for closing parenthesis
+    if (it->type != token_close_paren) {
+        return log_error("Expected \')\'");
+    }
     
-// }
+    it++; // Skip closing paren
 
-// };
+    return std::move(node);
+}
+
+expr_node_uptr parse_primary(token_it_ref it) {
+    switch (it->type) {
+    case token_variable:    return parse_variable_expr(it);
+    case token_number:  return parse_number_expr(it);
+    case token_open_paren:  return parse_paren_expr(it);
+    }
+
+    int token_function = token_normal_function | token_logarithmic_function | token_trigonometric_function;
+    if (it->type & token_function) {
+        return parse_function_expr(it);
+        
+    }
+
+    return log_error(std::format("unknown token \"{}\", expected an expression", it->value));
+}
+
+};
+
+namespace brose
+{
+
+void parse(const std::vector<Token>& tokens) {
+    
+}
+
+};
